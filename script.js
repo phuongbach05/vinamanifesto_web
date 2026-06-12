3b784f1b28cfe07436b5d0c24dd6b784fa96aec2
phuongbach05 <phuongbach0102@gmail.com>
2026-06-12 16:32:28 +0700

    Revert timeline and coordinate adjustments to commit bcb476

--- script.js
+++ script.js
@@ -286,12 +286,12 @@ const centers = {
   'taiwan': { cx: 265, cy: 815 },
   'vina-seat': { cx: 990, cy: 805 },
   'cay-neo': { cx: 650, cy: 805 },
-  'bds': { cx: 860, cy: 600 },
+  'bds': { cx: 1400, cy: 600 },
   'teaching': { cx: 1125, cy: 135 },
-  '2028': { cx: 1435, cy: 155 },
-  'open-source': { cx: 1435, cy: 485 },
+  '2028': { cx: 1545, cy: 155 },
+  'open-source': { cx: 1545, cy: 485 },
   'il-provino': { cx: 340, cy: 520 },
-  'vinav': { cx: 830, cy: 760 }
+  'vinav': { cx: 1130, cy: 760 }
 };
 
 const topicConnections = {
