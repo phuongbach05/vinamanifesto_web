b47c51f951b704bcbb57d3eab921edf2313ade87
phuongbach05 <phuongbach0102@gmail.com>
2026-06-12 16:49:36 +0700

    Shift VinA/V further right to cx: 1230 (boundary area)

--- script.js
+++ script.js
@@ -291,7 +291,7 @@ const centers = {
   '2028': { cx: 1545, cy: 155 },
   'open-source': { cx: 1545, cy: 485 },
   'il-provino': { cx: 340, cy: 520 },
-  'vinav': { cx: 1200, cy: 760 }
+  'vinav': { cx: 1230, cy: 760 }
 };
 
 const topicConnections = {
